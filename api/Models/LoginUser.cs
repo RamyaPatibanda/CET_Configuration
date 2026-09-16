9165e187cb4339a5c9bc842b68114f51ed0694a9
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-16 22:11:08 +0530

    Add login user model

---
+++ api/Models/LoginUser.cs
@@ -0,0 +1,13 @@
+namespace api.Models
+{
+    public class LoginUser
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
