1df24cbe8130eded06372f4c8382ecb686e0cf00
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-17 13:21:09 +0530

    Add derived field type to field source options

--- api/Models/FieldConfiguration/FieldSourceOption.cs
+++ api/Models/FieldConfiguration/FieldSourceOption.cs
@@ -3,5 +3,6 @@ namespace api.Models.FieldConfiguration
     public class FieldSourceOption
     {
         public string Name { get; set; } = string.Empty;
+        public string FieldType { get; set; } = string.Empty;
     }
 }
