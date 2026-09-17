de085ed240d547de9fcb933a4a59e5fe652b4076
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-17 11:39:58 +0530

    Add field configuration frontend service

---
+++ frontend/src/modules/fieldConfiguration/services/fieldConfigurationService.js
@@ -0,0 +1,46 @@
+import httpClient from "../../../services/httpClient";
+import API_ENDPOINTS from "../../../config/apiEndpoints";
+
+const fieldConfigurationService = {
+  async getFields() {
+    try {
+      return await httpClient.get(API_ENDPOINTS.FIELD_CONFIGURATION.LIST);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async getField(fieldId) {
+    try {
+      return await httpClient.get(API_ENDPOINTS.FIELD_CONFIGURATION.BY_ID(fieldId));
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async createField(field) {
+    try {
+      return await httpClient.post(API_ENDPOINTS.FIELD_CONFIGURATION.LIST, field);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateField(fieldId, field) {
+    try {
+      return await httpClient.put(API_ENDPOINTS.FIELD_CONFIGURATION.BY_ID(fieldId), field);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteField(fieldId) {
+    try {
+      return await httpClient.delete(API_ENDPOINTS.FIELD_CONFIGURATION.BY_ID(fieldId));
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default fieldConfigurationService;
