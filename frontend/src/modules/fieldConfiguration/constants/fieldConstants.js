be4ff10e659fda91d9732af52276cc4ef7cdab0f
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-17 11:40:03 +0530

    Add field configuration constants

---
+++ frontend/src/modules/fieldConfiguration/constants/fieldConstants.js
@@ -0,0 +1,7 @@
+export const FIELD_TYPES = [
+  { value: "Text", label: "Text" },
+  { value: "Number", label: "Number" },
+  { value: "Dropdown", label: "Dropdown" },
+  { value: "Date", label: "Date" },
+  { value: "Boolean", label: "Boolean" },
+];
