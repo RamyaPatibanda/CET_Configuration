cd84093fd5e37c581796a40d05e1077428b41ccf
Lakshmi Ramya <dhatasrip@talisma.com>
2026-09-17 11:31:21 +0530

    login working

--- api/DataAccess/CETDataAccess.cs
+++ api/DataAccess/CETDataAccess.cs
@@ -19,29 +19,36 @@ namespace api.DataAccess
 
         public async Task<LoginUser?> GetLoginUserAsync(string username)
         {
-            await using var connection = new SqlConnection(DBConnectionStr);
-            await using var command = new SqlCommand("sproc_GetLoginUser", connection)
+            try
             {
-                CommandType = CommandType.StoredProcedure,
-                CommandTimeout = 30
-            };
+                await using var connection = new SqlConnection(DBConnectionStr);
+                await using var command = new SqlCommand("sproc_GetLoginUser", connection)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = 30
+                };
 
-            command.Parameters.Add("@tUsername", SqlDbType.NVarChar, 100).Value = username;
-            await connection.OpenAsync();
+                command.Parameters.Add("@tUsername", SqlDbType.NVarChar, 100).Value = username;
+                await connection.OpenAsync();
 
-            await using var reader = await command.ExecuteReaderAsync();
-            if (!await reader.ReadAsync())
+                await using var reader = await command.ExecuteReaderAsync();
+                if (!await reader.ReadAsync())
+                    return null;
+
+                return new LoginUser
+                {
+                    UserId = reader.GetInt32(reader.GetOrdinal("aUserId")),
+                    Username = reader.GetString(reader.GetOrdinal("tUsername")),
+                    DisplayName = reader.GetString(reader.GetOrdinal("tDisplayName")),
+                    IsAdmin = reader.GetBoolean(reader.GetOrdinal("bIsAdmin")),
+                    Password = reader.GetString(reader.GetOrdinal("tPassword")),
+                    IsActive = reader.GetBoolean(reader.GetOrdinal("bIsActive"))
+                };
+            }
+            catch(Exception ex) {
+                var a = ex;
                 return null;
-
-            return new LoginUser
-            {
-                UserId = reader.GetInt32(reader.GetOrdinal("aUserId")),
-                Username = reader.GetString(reader.GetOrdinal("tUsername")),
-                DisplayName = reader.GetString(reader.GetOrdinal("tDisplayName")),
-                IsAdmin = reader.GetBoolean(reader.GetOrdinal("bIsAdmin")),
-                Password = reader.GetString(reader.GetOrdinal("tPassword")),
-                IsActive = reader.GetBoolean(reader.GetOrdinal("bIsActive"))
-            };
+            }
         }
     }
 }
