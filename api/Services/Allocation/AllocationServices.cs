ddcc1bed5a6cbfa9e15e627b5f91eb6a429b7656
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-18 12:20:34 +0530

    Move allocation context to dedicated model file

--- api/Services/Allocation/AllocationServices.cs
+++ api/Services/Allocation/AllocationServices.cs
@@ -1,18 +1,3 @@
-using api.Models.Allocation;
-
-namespace api.Services.Allocation;
-
-public sealed class AllocationContext
-{
-    public AllocationRun Run { get; init; } = new();
-    public IList<AllocationCandidate> Candidates { get; init; } = [];
-    public IList<SeatInventory> Seats { get; init; } = [];
-    public IReadOnlyDictionary<string, IReadOnlyList<AllocationRule>> RuleGroups { get; init; }
-        = new Dictionary<string, IReadOnlyList<AllocationRule>>(StringComparer.OrdinalIgnoreCase);
-    public IList<AllocationDecision> Decisions { get; } = [];
-    public IList<AllocationStageResult> StageResults { get; } = [];
-}
-
 public interface IAllocationStage
 {
     string StageCode { get; }
