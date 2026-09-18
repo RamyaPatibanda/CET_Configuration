45bd2381757dbddaf8d90049ddebb57f66ef24ea
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-18 18:19:04 +0530

    Remove duplicate rule condition operators

--- api/Models/RuleConfiguration/RuleCondition.cs
+++ api/Models/RuleConfiguration/RuleCondition.cs
@@ -11,9 +11,7 @@ namespace api.Models.RuleConfiguration
         public string Value { get; set; } = string.Empty;
         public int ConditionOrder { get; set; }
         public int GroupOrder { get; set; } = 1;
-        public string GroupLogicalOperator { get; set; } = "AND";
         public string ConditionLogicalOperator { get; set; } = "AND";
 
-        public string LogicalOperator { get; set; }
     }
 }
