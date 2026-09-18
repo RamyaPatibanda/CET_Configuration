90b856fe4e08a9441f20000cd7b8284cf7df617c
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-18 14:46:54 +0530

    Add user management data access

--- api/DataAccess/CETDataAccess.cs
+++ api/DataAccess/CETDataAccess.cs
@@ -51,5 +51,53 @@ namespace api.DataAccess
                 throw;
             }
         }
+        public async Task<IReadOnlyList<api.Models.UserManagement.UserDefinition>> GetUsersAsync()
+        {
+            var users = new List<api.Models.UserManagement.UserDefinition>();
+            await using var connection = new SqlConnection(DBConnectionStr);
+            await using var command = new SqlCommand("sproc_GetUsers", connection)
+            {
+                CommandType = CommandType.StoredProcedure,
+                CommandTimeout = 30
+            };
+
+            await connection.OpenAsync();
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                users.Add(new api.Models.UserManagement.UserDefinition
+                {
+                    UserId = reader.GetInt32(reader.GetOrdinal("aUserId")),
+                    Username = reader.GetString(reader.GetOrdinal("tUsername")),
+                    DisplayName = reader.GetString(reader.GetOrdinal("tDisplayName")),
+                    IsAdmin = reader.GetBoolean(reader.GetOrdinal("bIsAdmin")),
+                    IsActive = reader.GetBoolean(reader.GetOrdinal("bIsActive")),
+                    CreatedDate = reader.GetDateTime(reader.GetOrdinal("dtCreatedDate"))
+                });
+            }
+
+            return users;
+        }
+
+        public async Task<int> CreateUserAsync(string username, string encryptedPassword, string displayName, bool isAdmin, bool isActive)
+        {
+            await using var connection = new SqlConnection(DBConnectionStr);
+            await using var command = new SqlCommand("sproc_CreateUser", connection)
+            {
+                CommandType = CommandType.StoredProcedure,
+                CommandTimeout = 30
+            };
+
+            command.Parameters.Add("@tUsername", SqlDbType.NVarChar, 100).Value = username;
+            command.Parameters.Add("@tPassword", SqlDbType.NVarChar, 500).Value = encryptedPassword;
+            command.Parameters.Add("@tDisplayName", SqlDbType.NVarChar, 200).Value = displayName;
+            command.Parameters.Add("@bIsAdmin", SqlDbType.Bit).Value = isAdmin;
+            command.Parameters.Add("@bIsActive", SqlDbType.Bit).Value = isActive;
+
+            await connection.OpenAsync();
+            var result = await command.ExecuteScalarAsync();
+            return Convert.ToInt32(result);
+        }
+
     }
 }
