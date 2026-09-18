30bbfa344e362a3441eddb270161590bfd028fda
Lakshmi Ramya <dhatasrip@talisma.com>
2026-09-18 13:12:57 +0530

    changes

--- frontend/src/modules/ruleConfiguration/components/RuleForm.jsx
+++ frontend/src/modules/ruleConfiguration/components/RuleForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { FiEdit2, FiMenu, FiPlus, FiTrash2, FiUsers } from "react-icons/fi";
+import { FiEdit2, FiMenu, FiPlus, FiTrash2, FiUsers,FiCheckSquare } from "react-icons/fi";
 import Button from "../../../components/common/Button/Button";
 import Dialog from "../../../components/common/Dialog/Dialog";
 import Select from "../../../components/common/Select/Select";
