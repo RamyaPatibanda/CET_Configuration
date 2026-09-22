83f4a5a381786b76f6b826ee3fbb40393adf63b0
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-22 19:04:30 +0530

    Add permission catalog service

--- frontend/src/modules/userManagement/services/userManagementService.js
+++ frontend/src/modules/userManagement/services/userManagementService.js
@@ -3,6 +3,7 @@ import httpClient from "../../../services/httpClient";
 
 const userManagementService = {
   getUsers: () => httpClient.get(API_ENDPOINTS.USER_MANAGEMENT.LIST),
+  getPermissionCatalog: () => httpClient.get(API_ENDPOINTS.USER_MANAGEMENT.PERMISSION_CATALOG),
   createUser: (payload) => httpClient.post(API_ENDPOINTS.USER_MANAGEMENT.CREATE, payload),
 };
 
