13e9dcceeaaa453c9032d824144b9ce97a3089ed
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-22 21:54:46 +0530

    Allow frontend credentialed CORS requests

--- api/Program.cs
+++ api/Program.cs
@@ -44,7 +44,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:5173", "http://localhost:5174")
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
