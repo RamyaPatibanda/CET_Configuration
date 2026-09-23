245b6cb37cc407c41e2e1b8249341c26f4a403ff
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-23 19:02:03 +0530

    Add configurable IIS and API deployment settings

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./components/common/common.css";
 import App from "./App.jsx";
+import { getApplicationName } from "./config/runtimeConfig";
+
+document.title = getApplicationName();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
