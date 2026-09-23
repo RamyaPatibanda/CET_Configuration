4dec6c057c7446dfe015d99a4f6fe3e2d2af2b4a
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-23 19:07:35 +0530

    Support hosting React build under any IIS application path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // The application can be hosted under any IIS virtual application path.
+  base: "./",
 })
