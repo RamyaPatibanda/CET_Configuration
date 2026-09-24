62ac00e644392b5c2110ecbdaf723c452dd2be22
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-24 17:12:13 +0530

    Add user edit and delete API calls

--- frontend/src/modules/userManagement/services/userManagementService.js
+++ frontend/src/modules/userManagement/services/userManagementService.js
@@ -4,7 +4,7 @@ import httpClient from "../../../services/httpClient";
 const userManagementService = {
   getUsers: () => httpClient.get(API_ENDPOINTS.USER_MANAGEMENT.LIST),
   getPermissionCatalog: () => httpClient.get(API_ENDPOINTS.USER_MANAGEMENT.PERMISSION_CATALOG),
-  createUser: (payload) => httpClient.post(API_ENDPOINTS.USER_MANAGEMENT.CREATE, payload),
+  createUser: (payload) => httpClient.post(API_ENDPOINTS.USER_MANAGEMENT.CREATE, payload),\n  getUserPermissions: (userId) => httpClient.get(API_ENDPOINTS.USER_MANAGEMENT.PERMISSIONS(userId)),\n  updateUser: (userId, payload) => httpClient.put(API_ENDPOINTS.USER_MANAGEMENT.BY_ID(userId), payload),\n  deleteUser: (userId) => httpClient.delete(API_ENDPOINTS.USER_MANAGEMENT.BY_ID(userId)),
 };
 
 export default userManagementService;
