d09b786c552f06c8d9a01415a10a532a763554d4
RamyaPatibanda <130221793+RamyaPatibanda@users.noreply.github.com>
2026-09-24 17:12:33 +0530

    Add user edit and delete business operations

--- api/BusinessLogic/UserManagement/IUserManagementBL.cs
+++ api/BusinessLogic/UserManagement/IUserManagementBL.cs
@@ -5,6 +5,6 @@ namespace api.BusinessLogic.UserManagement;
 public interface IUserManagementBL
 {
     Task<IReadOnlyList<UserDefinition>> GetUsersAsync();
-    Task<int> CreateUserAsync(CreateUserRequest request);
+    Task<int> CreateUserAsync(CreateUserRequest request);\n    Task<IReadOnlyList<UserPermission>> GetUserPermissionsAsync(int userId);\n    Task UpdateUserAsync(int userId, UpdateUserRequest request);\n    Task DeleteUserAsync(int userId);
     Task<IReadOnlyList<UserPermission>> GetUserPermissionsAsync(int userId);
 }
